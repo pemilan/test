e964cc88d232dcaf4e1c2ef913d923c05e0e1d0e
m <m@m.m>
2018-02-05 09:04:17 +0100

    first stage

---
+++ foo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int foo(int i) {
+ return i;   
+}
+
+int main()  {
+int a=foo(3);
+printf("function foo() a=%d",a);
+printf("second line");
+printf("third line");
+printf("fourth line");
+}
+
