42a21fd34740e8023d04a92c1b956fc94dc651c2
Pe Milan <36397846+pemilan@users.noreply.github.com>
2018-02-27 12:49:57 +0100

    end of line

--- foo.c
+++ foo.c
@@ -7,19 +7,19 @@ int main()  {
 int a=foo(3);
 printf("function foo() a=%d\n",a);
 printf("second line\n");
-printf("third line");
-printf("fourth line");
-printf("fifth line");
+printf("third line\n");
+printf("fourth line\n");
+printf("fifth line\n");
 
-printf("Master commit");
-printf("Develop Branch");
+printf("Master commit\n");
+printf("Develop Branch\n");
 
-printf("Develop Branch1");
-printf("Develop commit");
-printf("foo");
-printf("try to merge");
-printf("try to merge to master");
-printf("Master Branch");
+printf("Develop Branch1\n");
+printf("Develop commit\n");
+printf("foo\n");
+printf("try to merge\n");
+printf("try to merge to master\n");
+printf("Master Branch\n");
 
 
 //no file
